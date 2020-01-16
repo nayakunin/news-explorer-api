599b26716abf88108041a6ad8614dcea17492148
nayakunin <nayakunin@outlook.com>
2020-01-16 22:11:15 +0300

    added models of article and user

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const AuthError = require('../errors/auth-err');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: (props) => `${props.value} Эта строка должна быть почтой!`,
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentionals = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Неправильные почта или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Неправильные почта или пароль');
+          }
+          return user;
+        });
+    });
+};
+
+
+module.exports = mongoose.model('user', userSchema);
