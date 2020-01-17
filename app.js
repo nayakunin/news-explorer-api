a2c7c8f8bdab77e5d57cf359cfa45069ef4b154c
nayakunin <nayakunin@outlook.com>
2020-01-17 17:29:32 +0300

    fixed a bug in error handler in app.js which was displaying internal errors to client

--- app.js
+++ app.js
@@ -51,10 +51,11 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
 });
 
 app.listen(PORT, () => {
