8a16cdeece5dd9ad91e86b81da42f97f69f05883
nayakunin <nayakunin@outlook.com>
2020-01-19 11:02:23 +0300

    added file which contains all error messages

---
+++ responses.js
@@ -0,0 +1,10 @@
+const error = {
+  articleNotFound: 'Article is not found',
+  userDoesNotExist: 'User does not exist',
+  noAuth: 'Authorization is needed',
+  singin: 'Wrong email or password',
+  resourceNotFound: 'Server can not find the requested resource',
+  serverInternal: 'Server error',
+};
+
+module.exports = error;
