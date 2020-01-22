33b5bba2b755abe4b8c5f8f7a61a2479dd5bce67
nayakunin <nayakunin@outlook.com>
2020-01-22 20:55:18 +0300

    added developer config to auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-err');
 const error = require('../responses');
 
-const { JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
+const { configJWTSecret } = require('../config');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +16,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : configJWTSecret);
   } catch (err) {
     throw new AuthError(error.noAuth);
   }
