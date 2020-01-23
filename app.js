51eaca5fd170e8d4e3edd75b543fffad4e5cb457
nayakunin <nayakunin@outlook.com>
2020-01-23 21:48:37 +0300

    moved signin and signup route to routes/index.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 require('dotenv').config();
 
 const { NODE_ENV, PORT = 3000, MONGODB_SERVER_ADDRESS } = process.env;
@@ -12,8 +12,6 @@ const app = express();
 
 const routes = require('./routes/index');
 
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const mainErrorHandler = require('./main_error_handler');
 const rateLimiter = require('./rate-limiter');
@@ -35,22 +33,6 @@ app.use(requestLogger);
 app.set('trust proxy', 1);
 
 app.use(rateLimiter);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-  }),
-}), createUser);
-
-app.use(auth);
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const userRouter = require('./users');
 const articleRouter = require('./articles');
+const { login, createUser } = require('../controllers/users');
+
+const auth = require('../middlewares/auth');
 
 const NotFoundError = require('../errors/not-found-err');
 const error = require('../responses');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+router.use(auth);
 
 router.use('/users', userRouter);
 router.use('/articles', articleRouter);
