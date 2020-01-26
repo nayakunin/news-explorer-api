1bdad6b2deba7a68e87bfde6b262afeb46e01b99
Nikita Yakunin <54310264+nayakunin@users.noreply.github.com>
2020-01-26 21:40:06 +0300

    Revert "Level 1"

--- config.js
+++
@@ -1,7 +0,0 @@
-const configJWTSecret = 'dev-ecrets';
-const configMongoDBServerAddress = 'mongodb://localhost:27017/newsdb';
-
-module.exports = {
-  configJWTSecret,
-  configMongoDBServerAddress,
-};
--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const userRouter = require('express').Router();
-
-const { getUserById } = require('../controllers/users');
-
-userRouter.get('/me', getUserById);
-
-module.exports = userRouter;
