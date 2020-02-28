3c38aedc55bd3f4fff0337b61386237a349c6de9
Nikita <nayakunin@outlook.com>
2020-02-28 14:37:14 +0300

    added http to localhost url

--- routes/cors.js
+++ routes/cors.js
@@ -4,7 +4,7 @@ const allowedCors = [
   // 'https://nayakunin-news-explorer.ru',
   // 'http://nayakunin-news-explorer.ru',
   // 'https://nayakunin.github.io',
-  'localhost:8080',
+  'http://localhost:8080',
 ];
 
 routerCors.use((req, res, next) => {
