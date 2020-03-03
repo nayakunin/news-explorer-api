be7b3dd485cff07cc91fa4b6020fba44ee8b9cad
Nikita <nayakunin@outlook.com>
2020-03-03 20:53:31 +0300

    changed cors

--- routes/cors.js
+++ routes/cors.js
@@ -11,7 +11,7 @@ routerCors.use((req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   next();
