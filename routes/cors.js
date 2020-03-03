d854c2e9123f3cb56c389fb71ebd17222ed9db5d
Nikita <nayakunin@outlook.com>
2020-03-03 21:29:29 +0300

    removed application/json from cors

--- routes/cors.js
+++ routes/cors.js
@@ -12,7 +12,7 @@ routerCors.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, application/json, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   }
 
