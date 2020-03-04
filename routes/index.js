28ece6b00b20da1abb0f95561ecc4675b9ec4473
Nikita <nayakunin@outlook.com>
2020-03-04 23:25:23 +0300

    removed cors require statement

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const cors = require('cors');
 const userRouter = require('./users');
 const articleRouter = require('./articles');
 const corsRouter = require('./cors');
